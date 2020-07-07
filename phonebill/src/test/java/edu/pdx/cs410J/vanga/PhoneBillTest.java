e02d44edc0611bea9a3f407658f4965edbd8d944
LikhithaVanga <likhithareddy1994@gmail.com>
2020-07-06 23:58:23 -0700

    commit

---
+++ phonebill/src/test/java/edu/pdx/cs410J/vanga/PhoneBillTest.java
@@ -0,0 +1,29 @@
+package edu.pdx.cs410J.vanga;
+
+import org.junit.Test;
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.nullValue;
+
+public class PhoneBillTest {
+    @Test
+    public void getCustomer() {
+        var name = "Likhitha";
+        PhoneBill bill = new PhoneBill(name);
+        assertThat(bill.getCustomer(), not(nullValue()));
+        assertThat(bill.getCustomer(), containsString(name));
+    }
+
+    @Test
+    public void getPhoneCalls() {
+        var name = "Likhitha";
+        PhoneBill bill = new PhoneBill(name);
+
+        var calls = bill.getPhoneCalls();
+
+        assertTrue(calls.isEmpty());
+    }
+
+}
